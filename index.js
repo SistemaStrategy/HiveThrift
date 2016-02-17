d59eeaaa2f4720bc4f0191357e3ca17089327038
Yann Blanc <yann.blanc@sistema-strategy.com>
2016-02-17 15:48:45 +0000

    First working Hive query, using thrift API for HiveServer2 (Hive 0.15.0, CDH 5.5.1.1, Thrift compiler 1.0.0-dev)

--- index.js
+++ index.js
@@ -3,45 +3,91 @@
 /**************/
 
 var thrift = require('thrift'),
-    Hive = require('./lib/gen-nodejs/TCLIService'),
+    hive = require('./lib/gen-nodejs/TCLIService'),
     ttypes = require('./lib/gen-nodejs/TCLIService_types');
 	
 /********************/
-/*Hive configuration*/
+/*Hive configuration, create config.json file*/
 /********************/
 
-var hiveHost = "vm-cluster2-node1",
-	hivePort = 10000,
-	hiveUser = 'root',
-	hivePassword = 'root';
-	
-/*****************/
-/*Client creation*/
-/*****************/
+var config = {
+	hiveHost : "vm-cluster2-node1",
+	hivePort : 10000,
+	hiveUser: 'root',
+	hivePassword : 'root',
+	auth : "nosasl",
+	timeout : "10000",
+}
 
-var connection = thrift.createConnection(hiveHost, hivePort),
-    client = thrift.createClient(Hive, connection);
+/*
+var config = JSON.parse(yield new Promise(function (resolve, reject) {
+    fs.readFile('./config.json', function (err, buf) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buf);
+      }
+	});
+}));*/
 	
+/************************************/
+/*Library definition, to externalize*/
+/************************************/
+
+function openSession(config, callback) {
+	openSessReq = new ttypes.TOpenSessionReq();
+	openSessReq.username = config.hiveUser;
+	openSessReq.password = config.hivePassword;
+	openSessReq.client_protocol = ttypes.TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V7;
+	//openSessReq.configuration = options;
+	console.log(openSessReq);
+	client.OpenSession(openSessReq, function (err, result){
+		if (err) {
+			console.log("OpenSession erreur : " + err);
+		} else {
+			console.log("OpenSession status : " + result.status.statusCode);
+			callback(result.sessionHandle);
+		}
+	});
+}
+
+function closeSession(session) {
+	client.CloseSession(session, function(err, result) {
+		if (err) {
+			console.log("closeSession :  erreur = " + error);
+		} else {
+			console.log('Session closed');
+			connection.end();
+			console.log('Connection closed');
+		}
+	});
+}
+
 /***********/
 /*Main code*/
 /***********/
 
+var connection = thrift.createConnection(config.hiveHost, config.hivePort);
+var client = thrift.createClient(hive, connection, {auth : config.auth, timeout : config.timeout});
+
 connection.on('error', function(err) {
-  console.error(err);
+	console.log('Connection error : ' + err);
+	/*Close the program with error code*/
+	process.exit(1)
 });
 
 connection.on('connect', function(){
-	console.log(connection);
-	/*Open session*/
-	openSessReq = new ttypes.TOpenSessionReq();
-	openSessReq.client_protocol = ttypes.TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V8;
-	openSessReq.username = hiveUser;
-	openSessReq.password = hivePassword;
-	var session = client.OpenSession(openSessReq);
-	console.log("Session : " + session);
-	/*Close session*/
+	console.log("OpenSession call");
+	openSession(config, function (session) {
+		console.log('Inside callback ... ');
+		var getInfoReq = new ttypes.TGetInfoReq();
+		getInfoReq.sessionHandle = session;
+		getInfoReq.infoType = ttypes.TGetInfoType.CLI_DBMS_NAME;
+		client.GetInfo(getInfoReq, function(errInfo, resInfo) {
+			console.log(JSON.stringify(resInfo));
+			client.CloseSession(session);
+			console.log("Program done");
+			process.exit(0);
+		});
+	});
 });
-
-/*Close connection ?*/
-
-
